472b27f68a77b1e3d4533e41c6ae809e6bb448cd
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-07-12 21:28:33 -0400

    Add files via upload
    Using middleware apps (body-parser)

--- Section 11 Setup a RESTful API from Scratch Node jQuery JavaScript SQLite/index.js
+++ Section 11 Setup a RESTful API from Scratch Node jQuery JavaScript SQLite/index.js
@@ -13,14 +13,19 @@ const site = http.createServer(function (req, res) {
 
 site.listen(3000);*/
 
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   res.send(`<h1>Hello World</h1>`);
 });
 
 app.post("/", function (req, res) {
+  console.log(req.body);
   res.send("POST sent");
 });
 
