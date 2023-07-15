4192ef7253461629df17ed5c8741788f746a7900
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-07-14 21:59:35 -0400

    Add files via upload
    simple login check

--- Section 11 Setup a RESTful API from Scratch Node jQuery JavaScript SQLite/index.js
+++ Section 11 Setup a RESTful API from Scratch Node jQuery JavaScript SQLite/index.js
@@ -16,23 +16,28 @@ site.listen(3000);*/
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const data = { user: "admin", pass: "password" };
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
+
+/*app.use(function (req, res, next) {
   console.log(`I'm in the middle`);
   console.log(req.body.username);
   console.log(req.body.password);
   next();
-});
+});*/
 
 app.get("/", function (req, res) {
-  res.send(`<h1>Hello World</h1>`);
+  res.send("GET sent");
 });
 
-app.post("/", function (req, res) {
-  console.log(req.body);
-  res.send("POST sent");
+app.post("/login", function (req, res) {
+  if (data.user == req.body.user && data.pass == req.body.pass) {
+    res.write('{"status":"success"}');
+  }
+  console.log(req.body.user);
+  res.send();
 });
 
 app.put("/", function (req, res) {
