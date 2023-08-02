a29d09333cda41c128ed0d8eebcf61a9ec58e478
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-08-01 20:50:07 -0400

    Add files via upload
    create elements

--- Promise Examples/app.js
+++ Promise Examples/app.js
@@ -1,10 +1,26 @@
-const url = "https://swapi.dev/api/planets";
-function fetchData() {
+const btn = document.createElement("button");
+btn.textContent = "press me";
+document.body.appendChild(btn);
+btn.addEventListener("click", function () {
+  fetchData("https://swapi.dev/api/planets");
+});
+const output = document.createElement("div");
+document.body.appendChild(output);
+function fetchData(url) {
   fetch(url)
     .then(function (rep) {
       return rep.json();
     })
     .then(function (data) {
+      output.textContent = `${data.count} results found`;
+      if (data.next) {
+        const btn2 = document.createElement("button");
+        btn2.textContent = "next";
+        output.appendChild(btn2);
+        btn2.addEventListener("click", function () {
+          fetchData(data.next);
+        });
+      }
       console.log(data);
     });
 }
